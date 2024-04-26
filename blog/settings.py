991344d55ec4e8b091d492b5ee5aeb33bd9e43fe
Leonardo Salvadori <leonardo.salvadori@franq.com.br>
2024-04-26 12:15:29 -0300

    updated allowed_hosts

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0=h*@6y%9pvun271mr$!sc!3%(-f4h1v=rmwn$ksen&dgdyq0o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
